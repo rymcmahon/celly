eaa617ea82b284d35e949fc771d34ae8b65d404a
rymcmahon <toedragdangle9@gmail.com>
2021-02-20 19:16:49 -0500

    added career and playoff stats with tests

--- lib/celly.rb
+++ lib/celly.rb
@@ -1,6 +1,63 @@
 require "celly/version"
+require 'net/http'
+require 'json'
 
 module Celly
   class Error < StandardError; end
-  # Your code goes here...
+
+  class Player
+    BASE_URL = 'https://statsapi.web.nhl.com/api/v1'
+    def self.profile(id)
+      end_point = "/people/#{id}"
+      uri = URI("#{BASE_URL}#{end_point}")
+      response = Net::HTTP.get_response(uri)
+
+      if response.code == '200'
+        json_response = JSON.parse(response.body)
+        {status: response.code, message: response.message, data: json_response["people"]}
+      else
+        {status: response.code, message: response.message}
+      end
+    end
+
+    def self.season_stats(player_id, season)
+      end_point = "/people/#{player_id}/stats?stats=statsSingleSeason&season=#{season}"
+      uri = URI("#{BASE_URL}#{end_point}")
+      response = Net::HTTP.get_response(uri)
+
+      if response.code == '200'
+        json_response = JSON.parse(response.body)
+        {status: response.code, message: response.message, data: json_response["stats"][0]["splits"][0]["stat"]}
+      else
+        {status: response.code, message: response.message}
+      end
+    end
+
+    def self.regular_season_career_stats(player_id)
+      end_point = "/people/#{player_id}/stats?stats=careerRegularSeason"
+      uri = URI("#{BASE_URL}#{end_point}")
+      response = Net::HTTP.get_response(uri)
+
+      if response.code == '200'
+        json_response = JSON.parse(response.body)
+        {status: response.code, message: response.message, data: json_response["stats"][0]["splits"][0]["stat"]}
+      else
+        {status: response.code, message: response.message}
+      end
+    end
+
+    def self.career_playoff_stats(player_id)
+      end_point = "/people/#{player_id}/stats?stats=careerPlayoffs"
+      uri = URI("#{BASE_URL}#{end_point}")
+      response = Net::HTTP.get_response(uri)
+
+      if response.code == '200'
+        json_response = JSON.parse(response.body)
+        {status: response.code, message: response.message, data: json_response["stats"][0]["splits"][0]["stat"]}
+      else
+        {status: response.code, message: response.message}
+      end
+    end
+
+  end
 end
--- spec/celly_spec.rb
+++ spec/celly_spec.rb
@@ -1,9 +1,28 @@
-RSpec.describe Celly do
-  it "has a version number" do
-    expect(Celly::VERSION).not_to be nil
+# RSpec.describe Celly do
+#   it "has a version number" do
+#     expect(Celly::VERSION).not_to be nil
+#   end
+
+#   it "does something useful" do
+#     expect(false).to eq(true)
+#   end
+# end
+
+RSpec.describe Celly::Player do
+  it "returns a 200 status when requesting a player by their id" do
+    expect(Celly::Player.profile(8478402)[:status]).to eq("200")
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  it "returns a 200 when requesting a player's single season stats" do
+    expect(Celly::Player.season_stats(8478402,20192020)[:status]).to eq("200")
+  end
+
+  it "returns a 200 when requesting a player's regular season career stats" do
+    expect(Celly::Player.regular_season_career_stats(8478402)[:status]).to eq("200")
+  end
+
+  it "returns a 200 when requesting a player's career playoffs stats" do
+    expect(Celly::Player.career_playoff_stats(8478402)[:status]).to eq("200")
   end
 end
+
